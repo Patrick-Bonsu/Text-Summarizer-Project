f37832c7167aa1b588581c457e9f8119a367d0cf
Patrick Yeboah <bonsuyeboahmurphy@gmail.com>
2023-11-10 22:30:47 +0000

    Data Ingestion notebook added

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path(r"C:\Users\USER\Desktop\Text Summarizer\Text-Summarizer-Project\config\config.yaml")
+PARAMS_FILE_PATH = Path(r"C:\Users\USER\Desktop\Text Summarizer\Text-Summarizer-Project\params.yaml")
+
